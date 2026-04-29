dd9df170f3aa70eeda1c324d1c7c8df639b66609
justynize <nwobajusticeekene12@gmail.com>
2026-04-29 10:45:48 +0100

    first commit

---
+++ src/route/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from '../Component/Navbar'
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Project</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Home
